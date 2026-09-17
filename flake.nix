92ad0744791e771f128606f5fa189e04b88721ab
snjy5 <snjy4@protonmail.com>
2026-09-17 22:43:28 +0530

    add(flake.nix): devshell

--- flake.nix
+++ flake.nix
@@ -1,27 +1,37 @@
 {
-    description = "My NixOS Flake";
+  description = "My NixOS Flake";
 
-    inputs = {
-        # Pin Nixpkgs to 26.05
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  inputs = {
+    # Pin Nixpkgs to 26.05
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
-        # Pin Home Manager to the EXACT SAME release (26.05)
-        home-manager = {
-            url = "github:nix-community/home-manager/release-26.05";
+    # Pin Home Manager to the EXACT SAME release (26.05)
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
 
-            # This line forces Home Manager to use YOUR nixpkgs input
-            # instead of downloading its own separate copy.
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+      # This line forces Home Manager to use YOUR nixpkgs input
+      # instead of downloading its own separate copy.
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = { nixpkgs, home-manager, ... }: {
-        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-                ./configuration.nix
-                home-manager.nixosModules.home-manager
-            ];
-        };
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+        ];
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ pkgs.certbot ];
+      };
     };
 }
